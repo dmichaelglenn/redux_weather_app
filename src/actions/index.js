23d58a8d2582d463daad613d7aec1b0564d701ab
Michael Glenn <dmichaelglenn@gmail.com>
2016-12-28 12:31:24 -0800

    preparing api request to weather api

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '0c3b4f5f00c29eca961ad8a7c46bd029';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
